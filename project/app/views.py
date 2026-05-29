21bc768bbf9fb29f5f78755bff7bd978b7ef38ad
GROHAAN <ag871998@gmail.com>
2026-05-29 17:52:28 +0530

    conditional statement if else and for loop

--- project/app/views.py
+++ project/app/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home (req):
-    return render(req, 'home.html')
+    data={
+        'age': 25
+    }
+    return render(req, 'home.html', data)
 
 def about (req):
-    return render(req, 'about.html')
+    data={
+        'students': ['abhishek', 'rohaan', 'virat','rohit']
+    }
+    return render(req, 'about.html', data)
